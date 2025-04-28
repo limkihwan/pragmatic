a413da09c60b3180a744fea9cf6168fd302de5f3
khlim <admin@admin.com>
2025-04-28 17:08:24 +0900

    django course 14 commit

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models import CharField
+
 
 # Create your models here.
+
+class HelloWorld(models.Model):
+    text = models.CharField(max_length=255, null=False)
+
+    def __str__(self):
+        return self.text
